e781d3fc9011de2f3ce2978258e5f97232c81dce
CharlesKibet <charlesbiegon973@gmail.com>
2024-06-05 11:48:20 +0300

    Python set up

---
+++ app.py
@@ -0,0 +1,16 @@
+#Database tables --> This is just where data is stored
+#When creating a table we use the CREATE TABLE table_name syntax
+#After the above syntax we should now provide what number of columns we want our table to have and the syntax is:
+#! CREATE TABLE cat(
+  # id INTEGER PRIMARY KEY AUTOINCREMENT,
+  # name TEXT NOT NULL,
+  # age INTEGER,
+  # species TEXT,
+#! );
+
+#!This alters the table by adding an extra column
+# We can also alter the table using the ALTER TABLE table_name ADD COLUMN column_name;
+
+#!Drop Table --> What this does basically is to delete the table from the database
+#DROP TABLE table_name;
+
