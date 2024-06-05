0892962d796fd69a025fdba98d15467ec42798ed
CharlesKibet <charlesbiegon973@gmail.com>
2024-06-05 12:04:30 +0300

    Creating tables in sql

--- sql/01_create_cats_table.sql
+++ sql/01_create_cats_table.sql
@@ -1,5 +1,9 @@
-CREATE TABLE cats (
+CREATE TABLE IF NOT EXISTS cats (
   id INTEGER PRIMARY KEY,
     name TEXT,
     age INTEGER
 );
+
+INSERT INTO cats (name ,age) VALUES("Pinky",12);
+
+---Data types in sqlite
